7a69eeb47a3b9b4894ec7e0db8068908a1727973
Asus <Asus@LAPTOP-0L1EL9LH>
2020-12-21 13:13:30 +0700

    Add repository konversi

---
+++ src/Konversi.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+
+/**
+ *
+ * @author nofairaamin
+ */
+public class Konversi {
+    public static void main (String[] args){
+        System.out.println("Pilihan Konversi:\n"
+                    + " 1. Jam -> Menit\n"
+                    + " 2. Detik -> Hari, Jam, menit dan detik");
+    
+        System.out.println("\nMasukkan pilihan");
+        try (Scanner options = new Scanner(System.in)) {
+            if (!options.hasNextInt()) {
+                System.out.println("Itu alfabet, anda salah!");
+            } else {
+                if (options.nextInt() == 1) {
+                    System.out.println("Masukkan Jam :");
+                    Scanner hours = new Scanner(System.in);
+                    //jamKeMenit (hours.nextInt ());
+                    jamKeMenit (hours.nextInt());
+            } else {
+                System.out.println("Masukkan detik :");
+                try (Scanner seconds = new Scanner (System.in)) {
+                     detikKeHari(seconds.nextLong());
+                     //secondsToDays(second.nextLong());           
+                }
+            }
+        }
+    }
+    }
+    
+    public static void jamKeMenit(long jam){
+        long menit = jam * 60;
+        System.out.println("Menit: " + menit);
+ }
+    public static void detikKeHari(long detik) {
+        //hari
+    int hari = (int) (detik/ (60 * 60 * 24));
+    
+    //jam
+    detik %= (60 * 60 * 24);
+    int jam = (int) (detik/ (60 * 60));
+    
+    //menit
+    detik %= (60 * 60);
+    int menit = (int) (detik/60);
+    
+    //detik
+    detik %= 60;
+    
+    System.out.println(hari + "Hari, " + jam + "jam, " + menit + "menit, " + detik + "detik");
+    }
+}
